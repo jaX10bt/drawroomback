2cc38eeb1450d47eab251275dbd14f62fe8e9fd1
Jakob Tulve <jakobtulve@gmail.com>
2023-11-30 14:58:42 +0200

    Initial commit

---
+++ src/test/java/com/example/drawroomback/DrawroombackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drawroomback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawroombackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
