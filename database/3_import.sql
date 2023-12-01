b83cb64550c95690f9a674f61a176090249e6df4
Jakob Tulve <jakobtulve@gmail.com>
2023-12-01 11:33:58 +0200

    created two test users for database

--- database/3_import.sql
+++ database/3_import.sql
@@ -1,2 +1,5 @@
 INSERT INTO project.role (id, name) VALUES (DEFAULT, 'admin');
 INSERT INTO project.role (id, name) VALUES (DEFAULT, 'customer');
+
+INSERT INTO project."user" (id, role_id, username, password, status) VALUES (DEFAULT, 1, 'admin', '123', 'A');
+INSERT INTO project."user" (id, role_id, username, password, status) VALUES (DEFAULT, 2, 'kasutaja', '123', 'A');
