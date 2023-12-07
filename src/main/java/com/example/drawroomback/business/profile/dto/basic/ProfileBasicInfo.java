267cac3fa405b4b207c1a44ac253002bd930f8e1
Ragnar Kaasik <ragnar.kaasik@gmail.com>
2023-12-07 10:50:51 +0200

    Getting profile info for profile view

--- src/main/java/com/example/drawroomback/business/profile/dto/basic/ProfileBasicInfo.java
+++
@@ -1,9 +0,0 @@
-package com.example.drawroomback.business.profile.dto.basic;
-
-import lombok.Data;
-
-@Data
-public class ProfileBasicInfo {
-    private Integer profileId;
-    private Integer userId;
-}
