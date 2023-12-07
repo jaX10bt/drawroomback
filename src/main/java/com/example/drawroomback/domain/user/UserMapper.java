dc1b6aad0725c56b26d4958649e6f7ccb8ee65f6
Ingrid <ingrid.lepik@gmail.com>
2023-12-07 11:27:59 +0200

    Added findAllActiveUsers.

--- src/main/java/com/example/drawroomback/domain/user/UserMapper.java
+++ src/main/java/com/example/drawroomback/domain/user/UserMapper.java
@@ -1,7 +1,10 @@
 package com.example.drawroomback.domain.user;
 
 import com.example.drawroomback.business.login.dto.LoginResponse;
+import com.example.drawroomback.business.users.dto.UserInfo;
 import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
@@ -9,5 +12,16 @@ public interface UserMapper {
     @Mapping(source = "id", target = "userId")
     @Mapping(source = "role.name", target = "roleName")
     LoginResponse toLoginResponse(User user);
+
+
+
+    @Mapping(source = "id", target = "userId")
+    @Mapping(source = "username", target = "username")
+    @Mapping(source = "status", target = "status")
+    UserInfo toUserInfo(User user);
+
+
+    List<UserInfo> toUserInfos(List<User> users);
+
 
 }
