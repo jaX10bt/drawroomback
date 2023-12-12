9761632108d4f9d31fbf8d2baab104343e14485f
Ingrid <ingrid.lepik@gmail.com>
2023-12-12 15:39:33 +0200

    Added @ApiResponse value

--- src/main/java/com/example/drawroomback/business/posts/PostsController.java
+++ src/main/java/com/example/drawroomback/business/posts/PostsController.java
@@ -4,6 +4,8 @@ import com.example.drawroomback.business.posts.dto.PostImage;
 import com.example.drawroomback.business.posts.dto.PostInfoRequest;
 import com.example.drawroomback.business.posts.dto.PostInfoResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.annotation.Resource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -29,6 +31,9 @@ public class PostsController {
     }
 
     @PostMapping("/post")
+    @Operation(summary = "Lisab uue postituse")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK")})
     public void addNewPost(@RequestBody PostInfoRequest postInfoRequest) {
         postsService.addNewPost(postInfoRequest);
     }
